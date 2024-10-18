b5ab9d398c49fc4f7e2e73a56003db087fa8e30d
hazemr2121 <ha.ahmed8996@gmail.com>
2024-10-18 06:56:25 +0300

    feat: fetch data using loaders and displaying loader indicator using useNavigation

---
+++ src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast react Pizza Co.</Link>
+    </header>
+  );
+}
+
+export default Header;
