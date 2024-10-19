e8c7822e42b8f853715ffedca13aac96fbe54cb0
hazemr2121 <ha.ahmed8996@gmail.com>
2024-10-19 11:23:06 +0300

    feat: reuse styles using react components on Cart button and home Button

---
+++ src/ui/LinkButton.jsx
@@ -0,0 +1,18 @@
+import { Link, useNavigate } from 'react-router-dom';
+function LinkButton({ children, to }) {
+  const className = 'text-sm text-blue-500 hover:text-blue-600';
+  const navigate = useNavigate();
+  if (to === '-1')
+    return (
+      <button onClick={() => navigate(-1)} className={className}>
+        {children}
+      </button>
+    );
+  return (
+    <Link to={to} className={className}>
+      {children}
+    </Link>
+  );
+}
+
+export default LinkButton;
